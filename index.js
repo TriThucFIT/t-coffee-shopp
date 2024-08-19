7cd03c599c6f90419bd2b8927e786b33ba9c443a
TriThucFIT <trithucbuittb@gmail.com>
2024-08-19 15:30:22 +0700

    Update CORS

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", allowedOrigins);
   if (allowedCors) {
     res.setHeader("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   }
   return next();
